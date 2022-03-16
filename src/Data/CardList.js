7db35737a9b4ccb4792ea6064173eeef642eec2b
domino659 <martin@sionfamily.com>
2022-03-16 17:33:13 +0100

    init

---
+++ src/Data/CardList.js
@@ -0,0 +1,22 @@
+export const CardList = [
+    {
+        id: 4,
+        title: 'quatrième titre',
+        content: "mais bon je te pardonne"
+    },
+    {
+        id: 3,
+        title: 'troisième titre',
+        content: "c'est vraiment navrant"
+    },
+    {
+        id: 2,
+        title: 'second titre',
+        content: "t'as vraiment pas bcp d'inspi pr les content"
+    },
+    {
+        id: 1,
+        title: 'premier titre',
+        content: "lorem mes couilles"
+    }
+];
