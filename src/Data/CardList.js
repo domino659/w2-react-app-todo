62e7cc9988964a5164e60b5ad7c0e37eb2836400
domino659 <martin@sionfamily.com>
2022-03-17 14:28:30 +0100

    ui

--- src/Data/CardList.js
+++ src/Data/CardList.js
@@ -2,21 +2,25 @@ export const CardList = [
     {
         id: 4,
         title: 'quatrième titre',
-        content: "mais bon je te pardonne"
+        content: "mais bon je te pardonne",
+        completed: false
     },
     {
         id: 3,
         title: 'troisième titre',
-        content: "c'est vraiment navrant"
+        content: "c'est vraiment navrant",
+        completed: true
     },
     {
         id: 2,
         title: 'second titre',
-        content: "t'as vraiment pas bcp d'inspi pr les content"
+        content: "t'as vraiment pas bcp d'inspi pr les content",
+        completed: false
     },
     {
         id: 1,
         title: 'premier titre',
-        content: "lorem mes couilles"
+        content: "lorem mes couilles",
+        completed: true
     }
 ];
